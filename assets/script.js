30bff27e351345d0d28218faf92f77b7a3833dc7
rm2023 <120582189+rm2023@users.noreply.github.com>
2023-03-06 16:07:34 -0500

    finally working

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-// variables for time
 
+// variables for timer
 var timeEl = document.querySelector(".time");
-var secondsLeft = 75;
+var secondsLeft = 20;
+var timerInterval
 
 function setTime() {
-  var timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = "Time " + secondsLeft;
 
@@ -14,11 +15,13 @@ function setTime() {
     }
   }, 1000);
 }
+//starting score
+score = 100;
 // Function to create message that games over
 function sendMessage() {
   timeEl.textContent = "GAME OVER ";
 }
-setTime();
+
 
 //Question BOX
 //variables for questions
@@ -60,8 +63,10 @@ a1.addEventListener("click", function () {
   if (a1.textContent == questions[currentQuestionIndex].correct) {
     YN.textContent = "correct";
     nextBtn.click();
+    
   } else {
     YN.textContent = "wrong";
+    score = score-25;
   }
 });
 
@@ -71,8 +76,11 @@ a2.addEventListener("click", function () {
   if (a2.textContent == questions[currentQuestionIndex].correct) {
     YN.textContent = "correct";
     nextBtn.click();
+    
+
   } else {
     YN.textContent = "wrong";
+    score = score-25;
   }
 });
 a3.addEventListener("click", function () {
@@ -82,6 +90,7 @@ a3.addEventListener("click", function () {
     nextBtn.click();
   } else {
     YN.textContent = "wrong";
+    score = score-25;
   }
 });
 
@@ -93,24 +102,44 @@ a4.addEventListener("click", function () {
   
   } else {
     YN.textContent = "wrong";
+    score = score-25;
   }
 });
 //next button
 nextBtn.addEventListener("click", function () {
   currentQuestionIndex++;
   YN.textContent = "";
-  if (questions[currentQuestionIndex] == undefined) {
+  if (currentQuestionIndex===0) {
+    setTime();
+  }
     
+  
+  if (questions[currentQuestionIndex] == undefined) {
+    clearInterval(timerInterval);
     questionE1.textContent = "game over";
+    
+    var scores=JSON.parse(localStorage.getItem("scores"))||[]
+    var user = prompt("CONGATULATIONS your score is "+score+" Please enter name");
+    var scoreObj={
+      score:score,
+      user:user
 
+    }
+    scores.push(scoreObj)
+    localStorage.setItem("scores",JSON.stringify(scores) );
+    window.location.href = "./score.html";
+    
+    
   } else {
+
     questionE1.textContent = questions[currentQuestionIndex].q;
     a1.textContent = questions[currentQuestionIndex].answers[0];
     a2.textContent = questions[currentQuestionIndex].answers[1];
     a3.textContent = questions[currentQuestionIndex].answers[2];
     a4.textContent = questions[currentQuestionIndex].answers[3];
+    nextBtn.style.display = "none";
     
     
   }
+  
 });
-
