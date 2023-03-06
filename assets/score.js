9429f4376a2a98a3c46d0b1201c661a6fc38cc55
rm2023 <120582189+rm2023@users.noreply.github.com>
2023-03-06 17:27:57 -0500

    last push

--- assets/score.js
+++ assets/score.js
@@ -1,14 +1,14 @@
 
 //had to be moved to another folder was keeping it from working
 //OPENS SCORES FROM STORAGE
-function highscore(){
-    var scores=JSON.parse(localStorage.getItem("scores"))||[] 
-    for (let index = 0; index < scores.length; index++) {
-      const element  = document.createElement("div");
-      element.textContent = scores[index].user+"-"+scores[index].score
-      var scoreEl = document.getElementById("scores")
-      scoreEl.appendChild(element)
-      console.log(scoreEl);
-    }
+function highscore() {
+  var scores = JSON.parse(localStorage.getItem("scores")) || []
+  for (let index = 0; index < scores.length; index++) {
+    const element = document.createElement("div");
+    element.textContent = scores[index].user + "-" + scores[index].score
+    var scoreEl = document.getElementById("scores")
+    scoreEl.appendChild(element)
+    console.log(scoreEl);
   }
-  highscore()
+}
+highscore()
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 
 // variables for timer
 var timeEl = document.querySelector(".time");
-var secondsLeft = 20;
+var secondsLeft = 30;
 var timerInterval
-
+//timer
 function setTime() {
   timerInterval = setInterval(function () {
     secondsLeft--;
@@ -26,12 +26,12 @@ function sendMessage() {
 //Question BOX
 //variables for questions
 var questionE1 = document.querySelector("#question-holder");
-var YN =document.querySelector("#Q")
-var a1= document.querySelector("#a1")
-var a2= document.querySelector("#a2")
-var a3= document.querySelector("#a3")
-var a4= document.querySelector("#a4")
-
+var YN = document.querySelector("#Q")
+var a1 = document.querySelector("#a1")
+var a2 = document.querySelector("#a2")
+var a3 = document.querySelector("#a3")
+var a4 = document.querySelector("#a4")
+//array inside object for questions and answers
 var nextBtn = document.querySelector(".show-next");
 var currentQuestionIndex = -1;
 var questions = [
@@ -44,92 +44,83 @@ var questions = [
     q: "What is the DOM?",
     answers: ["Document object model", "the man", "the lady", "a game"],
     correct: "Document object model"
-  },{
+  }, {
     q: "Which element is used for or styling HTML5 layout?",
     answers: ["JavaScrpt", "PHP", "jQuery", "CSS"],
     correct: "CSS"
-  },{
+  }, {
     q: " In which part of the HTML metadata is contained?",
     answers: ["head tag", "tite tag", "html tag", "body tag"],
     correct: "head tag"
   },
- 
+
 ];
-
-
-//clik button comparision
+//clik button comparision and score
 a1.addEventListener("click", function () {
- 
+
   if (a1.textContent == questions[currentQuestionIndex].correct) {
     YN.textContent = "correct";
     nextBtn.click();
-    
+
   } else {
     YN.textContent = "wrong";
-    score = score-25;
+    score = score - 25;
   }
 });
-
-
 a2.addEventListener("click", function () {
- 
+
   if (a2.textContent == questions[currentQuestionIndex].correct) {
     YN.textContent = "correct";
     nextBtn.click();
-    
-
   } else {
     YN.textContent = "wrong";
-    score = score-25;
+    score = score - 25;
   }
 });
 a3.addEventListener("click", function () {
- 
+
   if (a3.textContent == questions[currentQuestionIndex].correct) {
     YN.textContent = "correct";
     nextBtn.click();
   } else {
     YN.textContent = "wrong";
-    score = score-25;
+    score = score - 25;
   }
 });
 
 a4.addEventListener("click", function () {
- 
+
   if (a4.textContent == questions[currentQuestionIndex].correct) {
     YN.textContent = "correct";
     nextBtn.click();
-  
   } else {
     YN.textContent = "wrong";
-    score = score-25;
+    score = score - 25;
   }
 });
 //next button
 nextBtn.addEventListener("click", function () {
   currentQuestionIndex++;
   YN.textContent = "";
-  if (currentQuestionIndex===0) {
+  if (currentQuestionIndex === 0) {
     setTime();
   }
-    
-  
+
+  //first click function,prompts,scores,hides button,shows questions and answers
   if (questions[currentQuestionIndex] == undefined) {
     clearInterval(timerInterval);
     questionE1.textContent = "game over";
-    
-    var scores=JSON.parse(localStorage.getItem("scores"))||[]
-    var user = prompt("CONGATULATIONS your score is "+score+" Please enter name");
-    var scoreObj={
-      score:score,
-      user:user
 
+    var scores = JSON.parse(localStorage.getItem("scores")) || []
+    var user = prompt("CONGATULATIONS your score is " + score + " Please enter name");
+    var scoreObj = {
+      score: score,
+      user: user
     }
     scores.push(scoreObj)
-    localStorage.setItem("scores",JSON.stringify(scores) );
+    localStorage.setItem("scores", JSON.stringify(scores));
     window.location.href = "./score.html";
-    
-    
+
   } else {
 
     questionE1.textContent = questions[currentQuestionIndex].q;
@@ -138,8 +129,5 @@ nextBtn.addEventListener("click", function () {
     a3.textContent = questions[currentQuestionIndex].answers[2];
     a4.textContent = questions[currentQuestionIndex].answers[3];
     nextBtn.style.display = "none";
-    
-    
   }
-  
 });
